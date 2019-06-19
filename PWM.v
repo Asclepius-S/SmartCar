d3db52ee79df3c3a90252dbc7166dda4121cbc91
Asclepius-S <51984924+Asclepius-S@users.noreply.github.com>
2019-06-19 14:45:52 +0800

    Update PWM.v

--- PWM.v
+++ PWM.v
@@ -29,34 +29,34 @@ module PWM(
 	output reg [1:0] Led_Speed
     );
 	 
-	reg [19:0] count_for_pwmclk=0;  //pwm_clkµÄÀÛ¼ÓÆ÷
-	reg pwm_clk=0;                  //pwm_clkµÄĞÅºÅ
+	reg [19:0] count_for_pwmclk=0;  //pwm_clkçš„ç´¯åŠ å™¨
+	reg pwm_clk=0;                  //pwm_clkçš„ä¿¡å·
 	
-	always @(posedge clk)   //clkÊÇ50MhzµÄÊ±ÖÓĞÅºÅ
+	always @(posedge clk)   //clkæ˜¯50Mhzçš„æ—¶é’Ÿä¿¡å·
 	begin
 
         if(count_for_pwmclk == 20'b0000_0000_0000_1111_1010-1)
-		  begin    // 0.01ms´¥·¢Ò»´Î£¬¹Êpwm²¨ĞÎ¾«¶ÈÎª0.01ms 
+		  begin    // 0.01msè§¦å‘ä¸€æ¬¡ï¼Œæ•…pwmæ³¢å½¢ç²¾åº¦ä¸º0.01ms 
             count_for_pwmclk <= 0;
-            pwm_clk <= ~pwm_clk;   //°´Î»È¡·´
+            pwm_clk <= ~pwm_clk;   //æŒ‰ä½å–å
         end
         else
             count_for_pwmclk <= count_for_pwmclk + 1;
 	end
 	
 	reg [11:0] count_pwm=0;
-	reg [11:0] pwm_compare255=12'b0111_1101_0000;  //ËÙ¶È250PWM²¨±È½ÏÖµ
-	reg [11:0] pwm_compare250_Ori=12'b0111_1010_1001;  //ËÙ¶È250PWM²¨±È½ÏÖµ
-	reg [11:0] pwm_compare220_Ori=12'b0110_1011_1101;  //ËÙ¶È220PWM²¨±È½ÏÖµ
-	reg [11:0] pwm_compare150_Ori=12'b0100_1001_1000;  //ËÙ¶È150PWM²¨±È½ÏÖµ
+	reg [11:0] pwm_compare255=12'b0111_1101_0000;  //é€Ÿåº¦250PWMæ³¢æ¯”è¾ƒå€¼
+	reg [11:0] pwm_compare250_Ori=12'b0111_1010_1001;  //é€Ÿåº¦250PWMæ³¢æ¯”è¾ƒå€¼
+	reg [11:0] pwm_compare220_Ori=12'b0110_1011_1101;  //é€Ÿåº¦220PWMæ³¢æ¯”è¾ƒå€¼
+	reg [11:0] pwm_compare150_Ori=12'b0100_1001_1000;  //é€Ÿåº¦150PWMæ³¢æ¯”è¾ƒå€¼
 	reg [11:0] pwm_compare250, pwm_compare220, pwm_compare150;
 
-   reg pwm_flag255=0;
+        reg pwm_flag255=0;
 	reg pwm_flag250=0;  
 	reg pwm_flag220=0;
 	reg pwm_flag150=0;
 
-	always @(posedge pwm_clk)    //¿ØÖÆpwmĞÅºÅÊä³ö
+	always @(posedge pwm_clk)    //æ§åˆ¶pwmä¿¡å·è¾“å‡º
 	begin
 	   case(SpeedControl)
 		  2'b01:   
@@ -91,33 +91,33 @@ module PWM(
 	
 		count_pwm<=count_pwm+1;		
 		
-		if (count_pwm < pwm_compare255 && SpeedControl != 2'b00)   //ËÙ¶È255PWM²¨
+		if (count_pwm < pwm_compare255 && SpeedControl != 2'b00)   //é€Ÿåº¦255PWMæ³¢
 			pwm_flag255<=1;
 			else
 			pwm_flag255<=0;
 
-		if (count_pwm < pwm_compare250 && SpeedControl != 2'b00)   //ËÙ¶È250PWM²¨
+		if (count_pwm < pwm_compare250 && SpeedControl != 2'b00)   //é€Ÿåº¦250PWMæ³¢
 			pwm_flag250<=1;
 			else
 			pwm_flag250<=0;
 
-		if (count_pwm < pwm_compare220 && SpeedControl != 2'b00)   //ËÙ¶È220PWM²¨
+		if (count_pwm < pwm_compare220 && SpeedControl != 2'b00)   //é€Ÿåº¦220PWMæ³¢
 			pwm_flag220<=1;
 			else
 			pwm_flag220<=0;
 		
-		if (count_pwm < pwm_compare150 && SpeedControl != 2'b00)   //ËÙ¶È150PWM²¨
+		if (count_pwm < pwm_compare150 && SpeedControl != 2'b00)   //é€Ÿåº¦150PWMæ³¢
 			pwm_flag150<=1;
 			else
 			pwm_flag150<=0;
 
-		if (count_pwm == 12'b0111_1101_0000-1)  //Ã¿0.01ms¼ÓÒ»´Î£¬¼Ó2000´Î¼´Îª20msµÄÖÜÆÚ
+		if (count_pwm == 12'b0111_1101_0000-1)  //æ¯0.01msåŠ ä¸€æ¬¡ï¼ŒåŠ 2000æ¬¡å³ä¸º20msçš„å‘¨æœŸ
 			count_pwm<=0;
 
 	end
 
 assign pwm_255=pwm_flag255;
-assign pwm_250=pwm_flag250;   //pwm_flag¼´Îª¶ÔÓ¦µ½Êä³ö¿ÚµÄ¼Ä´æÆ÷
+assign pwm_250=pwm_flag250;   //pwm_flagå³ä¸ºå¯¹åº”åˆ°è¾“å‡ºå£çš„å¯„å­˜å™¨
 assign pwm_220=pwm_flag220;
 assign pwm_150=pwm_flag150;
 assign pwm_0=0;
